e7398a5c38690185a4dedc52fa6f26e4358da2ae
FY123 <47184257+ConquerorFY@users.noreply.github.com>
2024-10-02 11:14:09 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["github.com"], // Whitelist GitHub as an external image source
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
