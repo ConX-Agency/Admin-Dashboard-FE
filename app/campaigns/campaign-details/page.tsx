ce6b451e52ff32b8c2736e1e0ec780413f1e01b6
Sia-WRWD <chinojen7@gmail.com>
2024-11-03 23:03:38 +0800

    Fix issue with campaign details

--- app/campaigns/campaign-details/page.tsx
+++ app/campaigns/campaign-details/page.tsx
@@ -1,22 +1,36 @@
-"use client"
+"use client";
 
 import { CampaignDetailsContent } from "@/components/campaigns/CampaignDetailsContent";
 import { Separator } from "@/components/ui/separator";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
-const CampaignDetails = () => {
+const CampaignDetailsWrapper = () => {
   const searchParams = useSearchParams();
   const name = searchParams.get('name');
 
   return (
     <>
-      <div className="flex flex-row justify-between flex-wrap xxxs:gap-2">
-        <h1 className="text-3xl font-semibold items-center">{name}</h1>
-      </div>
-      <Separator className="mt-0 mb-0" />
-      <CampaignDetailsContent />
+      {name ? (
+        <>
+          <div className="flex flex-row justify-between flex-wrap xxxs:gap-2">
+            <h1 className="text-3xl font-semibold items-center">{name}</h1>
+          </div>
+          <Separator className="mt-0 mb-0" />
+          <CampaignDetailsContent />
+        </>
+      ) : (
+        <div className="text-gray-500">No campaign details available.</div>
+      )}
     </>
+  );
+};
+
+const CampaignDetails = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CampaignDetailsWrapper />
+    </Suspense>
   );
 };
 
