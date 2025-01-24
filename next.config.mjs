23e3bacae4e74b0b5d96ddd7db8a23970857343f
ConquerorFY <ryanrlfy33@gmail.com>
2025-01-24 13:12:00 +0800

    feat: added docker file

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["github.com", "static.wikia.nocookie.net", "static.vecteezy.com", "letsenhance.io"], // Whitelist GitHub as an external image source
+    // domains: ["github.com", "static.wikia.nocookie.net", "static.vecteezy.com", "letsenhance.io"], // Whitelist GitHub as an external image source
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*"
+      }
+    ]
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
